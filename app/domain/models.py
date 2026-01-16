eeea5f24e9a81f753be8ceace5bd1c58e9e59659
Sayantan Laha <lahasayantan1993@gmail.com>
2026-01-16 17:51:01 +0000

    clean code

--- app/domain/models.py
+++ app/domain/models.py
@@ -2,21 +2,6 @@ from dataclasses import dataclass
 from typing import Optional, Dict, Any, List, Union
 from datetime import datetime
 
-@dataclass
-class JobDescription:
-    crm_id: Optional[int] = None
-    title: Optional[str] = None 
-    role: Optional[str] = None  
-    description: Optional[Dict] = None    
-    skills: Optional[str] = None 
-    experience: Optional[int] = None 
-    location: Optional[str] = None 
-    is_active: Optional[bool] = None
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None    
-    created_by: Optional[str] = None
-    id: Optional[int] = None
- 
 @dataclass   
 class UserDetails:
     emp_id : Optional[int] = int
@@ -38,19 +23,13 @@ class SecurityQuestionList:
     question_list: Optional[List[SecurityQuestion]] = None
 
 @dataclass
-class UserRegister:
-    name: Optional[str] = str
-    email_id: Optional[str] = str
-    emp_id: Optional[int] = int
-    question_id_1: Optional[int] = int
-    answer_1: Optional[str] = str
-    question_id_2: Optional[int] = int
-    answer_2: Optional[str] = str
-
-@dataclass
-class UserCredential:
-    emp_id: Optional[int] = int
-    password: Optional[str] = str
+class AuthenticatedUser:
+    emp_id: int  = None
+    name: Optional[str] = None
+    role_id: Optional[int] = None
+    role_name: Optional[str] = None
+    is_first_login: Optional[bool] = None
+    is_approved: bool = None
 
 @dataclass
 class UserRole:
@@ -62,13 +41,9 @@ class UserRoleList:
     all_role: Optional[List[UserRole]] = None
 
 @dataclass
-class AuthenticatedUser:
-    emp_id: int  = None
-    name: Optional[str] = None
-    role_id: Optional[int] = None
-    role_name: Optional[str] = None
-    is_first_login: Optional[bool] = None
-    is_approved: bool = None
+class UserCredential:
+    emp_id: Optional[int] = int
+    password: Optional[str] = str
 
 @dataclass
 class ChangePassword:
@@ -96,14 +71,3 @@ class UserSecurityAnswer:
 class UserRoleUpdate:
     emp_id: str = None #Union[str, int] if needed
     role_id: Optional[int] = None
-
-@dataclass    
-class UpdateUserTlempid:
-    emp_id: str = None
-    tl_emp_id: int = None 
-
-@dataclass    
-class UpdateLobSpecializationIds:
-    emp_id: str = None
-    lob_id: int = None 
-    specialization_id: int = None
